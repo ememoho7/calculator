2271a2b8622ff96b9568269bf380dd1c53b61588
ememoho <ememoho@gmail.com>
2023-04-03 21:33:51 +0900

    Add icon.png file and push button with handler function in main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 # add library which needs for the application
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget   # application handler and blank GUI widget
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout,
+                             QMessageBox) # QMessageBox : MessageBox Widget
+from PyQt5.QtGui import QIcon   # library to add icon
 
 class Calculator(QWidget):  # define a class inherited QWidget class
 
@@ -12,9 +14,23 @@ class Calculator(QWidget):  # define a class inherited QWidget class
         self.initUI()   # initialize remains in initUI function
 
     def initUI(self):
+        self.btn1 = QPushButton('Message', self)    # add button
+        self.btn1.clicked.connect(self.activateMessage) # connect handler function when button clicked
+
+        vbox=QVBoxLayout()  # create vertical layout widget
+        vbox.addStretch(1)  # empty space
+        vbox.addWidget(self.btn1)   # button location
+        vbox.addStretch(1)  # empty space
+
+        self.setLayout(vbox)    # empty space - button - empty space
+
         self.setWindowTitle('Calculator')   # sete the window title
+        self.setWindowIcon(QIcon('icon.png'))   # add window icon
         self.resize(256, 256)   # window size
         self.show() # call for window display
+
+    def activateMessage(self): # function when button clicked: Message Box output
+        QMessageBox.information(self, "information", "Button clicked!")
 
 
 if __name__ == '__main__':  # pyqt needs 1 QApplication per appplication
