d4ad3321e48739ca427ae4e13346fcf97bec1af4
ememoho <ememoho@gmail.com>
2023-04-09 14:53:12 +0900

    Refactor main.py to be implemented in ui.py and ctrl.py

--- main.py
+++ main.py
@@ -1,55 +1,15 @@
 # ch 4.2.1 main.py
-
-# add library which needs for the application
 import sys
+from ui import View # add View class of ui.py
+from ctrl import Control    # add Control class of ctrl.py
+from PyQt5.QtWidgets import QApplication
 
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout,
-                             QMessageBox, QPlainTextEdit, QHBoxLayout) # add QHBoxLayout
-from PyQt5.QtGui import QIcon   # library to add icon
+def main(): # function for program excution(Application)
+    calc = QApplication(sys.argv)
+    app = QApplication(sys.argv)
+    view = View()
+    Control(view = view)
+    sys.exit(app.exec_())
 
-class Calculator(QWidget):  # define a class inherited QWidget class
-
-    def __init__(self):
-        super().__init__()  # initialize QWidget parent class
-        self.initUI()   # initialize remains in initUI function
-
-    def initUI(self):
-        self.te1 = QPlainTextEdit() # create text edit widget
-        self.te1.setReadOnly(True)  # modify text edit widget for read only
-
-        self.btn1 = QPushButton('Message', self)    # add button
-        self.btn1.clicked.connect(self.activateMessage) # connect handler function when button clicked
-
-        self.btn2 = QPushButton('Clear', self)    # add btn2
-        self.btn2.clicked.connect(self.clearMessage) # connect btn2 handler function
-
-        hbox = QHBoxLayout()    # add horizontal box layout and add btn1, 2
-        hbox.addStretch(1)  # space
-        hbox.addWidget(self.btn1)
-        hbox.addWidget(self.btn2)
-
-        vbox=QVBoxLayout()  # create vertical layout widget
-        vbox.addWidget(self.te1)    # add text edit widget at vertial layout
-        # vbox.addWidget(self.btn1)   # button location
-        vbox.addLayout(hbox)
-        vbox.addStretch(1)  # empty space
-
-        self.setLayout(vbox)    # empty space - button - empty space
-
-        self.setWindowTitle('Calculator')   # sete the window title
-        self.setWindowIcon(QIcon('icon.png'))   # add window icon
-        self.resize(256, 256)   # window size
-        self.show() # call for window display
-
-    def activateMessage(self): # modify handler function: to output message in text edit
-        # QMessageBox.information(self, "information", "Button clicked!")
-        self.te1.appendPlainText("Button clicked!")
-
-    def clearMessage(self):
-        self.te1.clear()
-
-
-if __name__ == '__main__':  # pyqt needs 1 QApplication per appplication
-    app = QApplication(sys.argv)    # create QApplication instance
-    view = Calculator() # create Calculator window instance
-    sys.exit(app.exec_())   # loop for application event handling
+if __name__ == '__main__':
+    main()
