58f0a913ab54a0cfa393f68fa425ff9970c19ad5
ememoho <ememoho@gmail.com>
2023-04-16 18:29:53 +0900

    Modify ui.py to add le1, le2 and cb widgets

--- ui.py
+++ ui.py
@@ -1,7 +1,9 @@
-# ch 5.2.1 ui.py
+# ch 6.2.1 ui.py
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout,
-                             QMessageBox, QPlainTextEdit, QHBoxLayout)
+                             QMessageBox, QPlainTextEdit, QHBoxLayout,
+                             QLineEdit, QComboBox) # add QLineEdit, QComboBox 
 from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore # add module
 
 class View(QWidget):
     def __init__(self):
@@ -11,6 +13,20 @@ class View(QWidget):
     def initUI(self):
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
+
+        self.le1 = QLineEdit('0', self) # add line edit1
+        self.le1.setAlignment(QtCore.Qt.AlignRight) # set align of line edit1
+
+        self.le2 = QLineEdit('0', self) # add line edit2
+        self.le2.setAlignment(QtCore.Qt.AlignRight) # set align of line edit2
+
+        self.cb = QComboBox(self) # add combo box
+        self.cb.addItems(['+', '-', '*', '/']) # add items to combo box (use as operators)
+
+        hbox_formular = QHBoxLayout()
+        hbox_formular.addWidget(self.le1)
+        hbox_formular.addWidget(self.cb)
+        hbox_formular.addWidget(self.le2)
 
         self.btn1 = QPushButton('Message', self)
         self.btn2 = QPushButton('Clear', self)
@@ -22,6 +38,7 @@ class View(QWidget):
 
         vbox = QVBoxLayout()
         vbox.addWidget(self.te1)
+        vbox.addLayout(hbox_formular)   # place hbox_formular
         vbox.addLayout(hbox)
         vbox.addStretch(1)
 
